2dc2b7e8d3e2d5ff7bd44e50df3c5a839e694eb8
jamanumass <59070429+jamanumass@users.noreply.github.com>
2020-01-30 13:06:56 -0500

    Add files via upload
    updated, working on all angiosperms, made for CLV1

--- angiosperm_1KT_HMM_search.sh
+++ angiosperm_1KT_HMM_search.sh
@@ -14,17 +14,27 @@ module load hmmer/3.1b2
 module load blast/2.2.22
 module load noisy/1.5.12
 module load iqtree/1.6.3
+module load R/3.6.1_packages/tidyverse/1.3.0 gcc/8.1.0
 
-<<COMMENT
-echo "building database to find priors..."
+run="CLV1"
+
+
+
+#<<COMMENT
+
+
+##this section will take a list of genes, make an HMM, search all 1kt and n best hit collect sequences, then scan them for domain content
+cd /home/jm33a/domain_evolution/1KT_searches/$run/
+
+echo "constructing database for input sequences, collecting and aligning inputs..."
     cat $(ls ~/supertree/databases/primary_transcript_databases/*.oneline.fa) > combined_onelines.fa
-    database=combined_onelines.fa
-echo "collecting prior sequences from database..."
-    grep -w -A 1 -f clade_input_IDs.txt --no-group-separator $database | awk '{print $1}' > input_seqs.fa #collect sequences of inputs
-echo "Aligning prior sequences..."
-    mafft input_seqs.fa > input_alignment.fasta #align sequences
-echo "building an HMM from the alignment..."
-    hmmbuild -o hmmout.txt model.hmm input_alignment.fasta #build HMM of clade
+    grep -w -A 1 -f *geneIDs.txt --no-group-separator combined_onelines.fa | awk '{print $1}' > $run.input_seqs.fa #collect input clade seqs
+    rm combined_onelines.fa
+    linsi $run.input_seqs.fa > $run.input_align.fasta
+
+echo "make $run hmm from input alignment..."
+	hmmbuild -o hmmout.txt $run.model.hmm $run.input_align.fasta
+	rm hmmout.txt
 
 
 echo "collect the top 2 hits for each of the species listed in flowering_plants_species_IDs"
@@ -37,67 +47,82 @@ do
     test -f $protein_database_path #test if file exists. Write 0 to $? if yes, 1 if no
     if [ $? -eq 0 ] #only perform the search when a database exists, otherwise report the error
     then
-        hmmsearch -o hmmout.txt --noali --tblout table.output.txt model.hmm $protein_database_path #scan all sequences from a species with the HMM model
+        hmmsearch -o hmmout.txt --noali --tblout table.output.txt $run.model.hmm $protein_database_path #scan all sequences from a species with the HMM model
     
         #next collec the sequence of the top n hits found in the search
         for hit_number in {4..5} #top hit is on line 4, specify how many lines after that
         do
             hit=$(sed -n "${hit_number}p" < table.output.txt | awk '{print $1}') #gene ID from hit number line
             hit_full_ID=">${species_ID}_${hit}" #concatenate the species ID and the geneID with an underscore between, using fasta format >
-            echo $hit_full_ID >> top_hits.seqs.fa #add gene full gene ID to fasta sequences
-            grep -w -A 1 $hit $protein_database_path | tail -n 1 >> top_hits.seqs.fa #add gene's sequence to running list
+            echo $hit_full_ID >> $run.top_hits.seqs.fa #add gene full gene ID to fasta sequences
+            grep -w -A 1 $hit $protein_database_path | tail -n 1 >> $run.top_hits.seqs.fa #add gene's sequence to running list
         done
     else
         echo "$species_ID does not have a peptide database"
     fi
     
-done < flowering_plants_species_IDs
+done < /home/jm33a/domain_evolution/flowering_plants_species_IDs
 
 #clean up a bit before proceeding
-rm $database
 rm hmmout.txt
 rm table.output.txt
 
-#species_ID=WAIL #nothing
-#species_ID=WKSU #something
-
-
-#next use all hits to infer a tree
 echo "removing hits with sequences that were not found"
-    grep -v "#" top_hits.seqs.fa > top_hits_clean.fa
-    rm top_hits.seqs.fa
-    cat top_hits_clean.fa > top_hits.seqs.fa
+    grep -v "#" $run.top_hits.seqs.fa > top_hits_clean.fa
+    rm $run.top_hits.seqs.fa
+    cat top_hits_clean.fa > $run.top_hits.seqs.fa
     rm top_hits_clean.fa
 
 
-echo "adding original clade sequences to hits from 1KT, storing as tree_seqs.fa"
-    cat top_hits.seqs.fa input_seqs.fa > tree_seqs.fa
-echo "Aligning..."
-    mafft tree_seqs.fa > tree_alignment.fasta
-echo "Filtering with noisy..."
-    noisy -s tree_alignment.fasta 
-
-COMMENT
-
-echo "inferring tree..."
-#    iqtree -s *out.fas -bb 1000 -nt 16 -m LG+F+R7
-    iqtree -s *out.fas -fast -nt 16 -m LG+F+R7
-    
+#this section to remove any genes for which both an LRR and RLK domain are not found
+echo "Building Pfam domain table from genes" 	##in case need to regenerate pfam searchable database: $ hmmpress Pfam-A.hmm top_hits.seqs.fa
+	hmmscan --noali -o pfamout.temp --cut_tc --tblout $run.pfamout.tsv ~/pfam/hmmfiles/Pfam-A.hmm $run.top_hits.seqs.fa 
+	rm pfamout.temp
+echo "use R script to process this list to only genes with both domains, and only the gene IDs. output is $run.both_domains_IDs.txt"
+    Rscript /home/jm33a/domain_evolution/sort_full_LRR_RLKs_olnly.R $run
 
 
-#remove iqtree log files
-rm *splits.nex
-rm *ckp.gz
-rm *bionj
-#rm *treefile
-rm *mldist
-rm *.log
-rm *.iqtree
-rm *model.gz
-rm *uniqueseq.phy
-rm *idx.txt
-rm *sta.gr
-rm *typ.eps
+
+echo "use the filtered list of gene IDs to collect sequence, make an alignment and tree"
+fgrep -w --no-group-separator -A 1 -f $run.hits_with_both_domains_IDs.txt $run.top_hits.seqs.fa > $run.hits_with_both_domains_seqs.fa #pull only seqs from R screening
+mkdir tree_from_hits
+cp $run.hits_with_both_domains_seqs.fa tree_from_hits
+cd tree_from_hits
+cat /home/jm33a/domain_evolution/scaffold_seqs.fa $run.hits_with_both_domains_seqs.fa >> $run.hits_and_scaffold_seqs.fa #add the backbone scaffold sequences to tree
+
+mafft --thread 16 $run.hits_and_scaffold_seqs.fa > $run.hits_and_scaffold_align.fasta #quick version
+
+echo "running noisy to clean up alignment..."
+	noisy -s --noconstant $run.hits_and_scaffold_align.fasta
+	#remove noisy log files
+	rm *typ.eps
+	rm *sta.gr
+	rm *idx.txt
+
+
+
+echo "Running IQtree with auto find model of evo and inference of site rate evolution.."
+	iqtree -s *out.fas -fast -nt 16 -m LG+F+R7 #quick version
+	#iqtree -s *out.fas -bb 1000 -wsr -nt 16 -m LG+F+R7 #full version, infer site rates
+
+
+echo "cleaning up.."
+		#remove iqtree log files
+	rm *splits.nex
+	rm *ckp.gz
+	rm *bionj
+	rm *mldist
+	rm *.log
+	rm *.iqtree
+	rm *model.gz
+	rm *uniqueseq.phy
+
+exit
+
+
+
+
+
 
 
 
